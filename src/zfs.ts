9eb6bd9b389132c679ce89dba9076ba7dca034cf
Rexford Essilfie <rexfordessilfie09@gmail.com>
2023-03-08 11:19:53 -0600

    Support more zod methods (#5)
    * feat: add more zod direct method creators
    
    * feat: zfs early throw on invalid creator members
    
    * chore: bump minor version
    
    * test: update zfs error messages and throw logic
    
    * chore: sync package.json and package-lock.json

--- src/zfs.ts
+++ src/zfs.ts
@@ -10,24 +10,34 @@ export function zodFactorySerial<T extends AllLazyMembers>(
   const [_initialMember, ...initialMemberArgs] = first || [];
 
   let initialMember = _initialMember as keyof OnlyMethods<typeof zf>;
+  const creator = zf[initialMember];
 
-  const initialExpression = zf[initialMember](...initialMemberArgs);
+  if (typeof creator !== "function") {
+    throw new Error(`Unrecognized initial member: ${initialMember}`);
+  }
+
+  const initialExpression = creator(...initialMemberArgs);
 
   return rest.reduce((acc, curr) => {
     const [currMethod, ...currArgs] = curr;
 
     const typeMethodKey = `${acc._zfType}` as const;
-    if (typeMethodKey in zf) {
-      const typeMethods = zodSubMemberCreators[typeMethodKey];
-      if (typeMethods) {
-        if (currMethod in typeMethods) {
-          const creator = typeMethods[currMethod as keyof typeof typeMethods];
-          //@ts-ignore
-          const result = creator(acc, ...currArgs);
-          return result;
-        }
-      }
+
+    if (!(typeMethodKey in zf)) {
+      throw new Error(`Unrecognized _zfType for expression: ${acc._zfType}`);
     }
+
+    const typeMethods = zodSubMemberCreators[typeMethodKey];
+
+    if (!(currMethod in typeMethods)) {
+      throw new Error(`Unrecognized sub member: ${currMethod}`);
+    }
+
+    const creator = typeMethods[currMethod as keyof typeof typeMethods];
+
+    //@ts-ignore
+    const result = creator(acc, ...currArgs);
+    return result;
 
     return acc;
   }, initialExpression as ts.Expression & { _zfType: keyof typeof zodSubMemberCreators });
